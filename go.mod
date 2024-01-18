4b3f4cecd525ef61b9fd8801b78c1245a245d539
lucia <llcsaturnaliam@gmail.com>
2024-01-18 10:20:05 -0600

    added decompression removed hardcoded files

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module swf-extract
+module swfextract
 
 go 1.21.5
--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"compress/zlib"
 	"encoding/binary"
+	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 )
 
+// reads a file as an array of bytes
 func readFileBytes(filename string) []byte {
 	file, err := os.Open(filename)
 	var fileBytes []byte
@@ -33,6 +38,50 @@ func readFileBytes(filename string) []byte {
 	return fileBytes
 }
 
+func decompress(data []byte) []byte {
+	// adding the zlib header
+	data = append(data, 0x78)
+	data = append(data, 0x1)
+
+	buffer := bytes.NewBuffer(data)
+
+	r, err := zlib.NewReader(buffer)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	var decompressedBytes bytes.Buffer
+
+	io.Copy(&decompressedBytes, r)
+
+	return decompressedBytes.Bytes()
+}
+
 func main() {
-	readFileBytes("pvz_9_15.swf")
+	// parsing the arguments passed to the program
+	commandLineArgs := os.Args
+
+	if len(commandLineArgs) != 2 {
+		fmt.Printf("usage: %s <file>\n", filepath.Base(commandLineArgs[0]))
+		os.Exit(1)
+	}
+
+	fileBytes := readFileBytes(commandLineArgs[1])
+
+	// getting the easily accessible data
+	signature := fileBytes[:3]
+	compressionSignature := fileBytes[0]
+	version := fileBytes[3]
+	bytesSize := binary.LittleEndian.Uint32(fileBytes[4:8])
+
+	if fileBytes[0] == 'C' {
+		decompressed := decompress(fileBytes[8:])
+		fmt.Printf("%s %c %d %d %x", signature, compressionSignature, version, bytesSize, decompressed[0])
+	}
+
 }

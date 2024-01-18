cbeb14aaa376330d1542e85558d069a7be5413e7
lucia <llcsaturnaliam@gmail.com>
2024-01-18 11:24:32 -0600

    rect processing

--- main.go
+++ main.go
@@ -8,7 +8,17 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 )
+
+type Rect struct {
+	Nbits uint64
+	Xmin  int64
+	Xmax  int64
+	Ymin  int64
+	Ymax  int64
+}
 
 // reads a file as an array of bytes
 func readFileBytes(filename string) []byte {
@@ -62,6 +72,32 @@ func decompress(data []byte) []byte {
 	return decompressedBytes.Bytes()
 }
 
+func fmtBits(data []byte) []byte {
+	var buf bytes.Buffer
+	for _, b := range data {
+		fmt.Fprintf(&buf, "%08b ", b)
+	}
+	buf.Truncate(buf.Len() - 1) // To remove extra space
+	return buf.Bytes()
+}
+
+// gets a rect structure
+func getRect(data []byte) Rect {
+	var rectData Rect
+
+	bytesSlice := data[:9]
+	binaryData := string(fmtBits(bytesSlice))
+	binaryData = strings.Replace(binaryData, " ", "", -1)
+
+	rectData.Nbits, _ = strconv.ParseUint(binaryData[:5], 2, 8)
+	rectData.Xmin, _ = strconv.ParseInt(binaryData[5:20], 2, 15)
+	rectData.Xmax, _ = strconv.ParseInt(binaryData[20:35], 2, 15)
+	rectData.Ymin, _ = strconv.ParseInt(binaryData[35:50], 2, 15)
+	rectData.Ymax, _ = strconv.ParseInt(binaryData[50:65], 2, 15)
+
+	return rectData
+}
+
 func main() {
 	// parsing the arguments passed to the program
 	commandLineArgs := os.Args
@@ -79,9 +115,21 @@ func main() {
 	version := fileBytes[3]
 	bytesSize := binary.LittleEndian.Uint32(fileBytes[4:8])
 
-	if fileBytes[0] == 'C' {
-		decompressed := decompress(fileBytes[8:])
-		fmt.Printf("%s %c %d %d %x", signature, compressionSignature, version, bytesSize, decompressed[0])
+	var compressionType string
+
+	// decompressing
+	if compressionSignature == 'C' {
+		compressionType = "zlib compressed"
+
+		fileBytes = decompress(fileBytes[8:])
+	} else if compressionSignature == 'F' {
+		fileBytes = fileBytes[8:]
+		compressionType = "uncompressed"
+	} else {
+		compressionType = "LZMA compressed"
 	}
 
+	getRect(fileBytes)
+
+	fmt.Printf("Signature: %s\nCompression type: %c (%s)\nSWF version: %d\nUncompressed size (bytes): %d\n", signature, compressionSignature, compressionType, version, bytesSize)
 }
